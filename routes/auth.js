0199fce5c02f31d62bc92aab900b027c3735f4f4
Sentpiqe <sentpiqe@proton.me>
2022-08-05 14:49:24 -0500

    remove redirect

--- routes/auth.js
+++ routes/auth.js
@@ -149,7 +149,15 @@ router.post(
   "/login",
   passport.authenticate("local", { failureRedirect: "/auth/login/failed" }),
   function (req, res) {
-    res.redirect("/auth/login/success");
+    // res.redirect("/auth/login/success");
+    if (req.user) {
+      res.json({
+        success: true,
+        message: "user has successfully authenticated",
+        user: req.user,
+        cookies: req.cookies,
+      });
+    }
   }
 );
 
