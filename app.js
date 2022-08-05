b7c2cf843c9c744f01504f594119227bf8cfec8d
Sentpiqe <sentpiqe@proton.me>
2022-08-05 14:07:45 -0500

    whitelist solution

--- app.js
+++ app.js
@@ -21,14 +21,25 @@ app.use(
     name: "session",
     keys: [process.env.COOKIE_KEY],
     maxAge: 24 * 60 * 60 * 100,
-    sameSite: 'none'
+    sameSite: "none",
   })
 );
+
+var allowedDomains = [process.env.CLIENT_HOME_PAGE_URL, process.env.API_URL];
 
 // CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_HOME_PAGE_URL, // allow to server to accept request from different origin
+    origin: function (origin, callback) {
+      // bypass the requests with no origin (like curl requests, mobile apps, etc )
+      if (!origin) return callback(null, true);
+
+      if (allowedDomains.indexOf(origin) === -1) {
+        var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }, // allow to server to accept request from different origin
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // allow session cookie from browser to pass through
   })
