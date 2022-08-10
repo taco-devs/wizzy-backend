e3996550f69616c88ba21ce72d71ba8a295b57dc
Sentpiqe <sentpiqe@proton.me>
2022-08-10 17:23:03 -0500

    debug

--- app.js
+++ app.js
@@ -78,7 +78,6 @@ app.use(bodyparser.json());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
